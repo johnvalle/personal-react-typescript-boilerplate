970539c8f77fe0587ae2640f61afd6b1415602f7
John Valle <vallejohn.vj@gmail.com>
2021-09-10 23:00:31 +0800

    Removed webVitals

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 
 import * as Pages from "./pages";
 
 function App() {
+  /**
+   * A new query client is set and provided to
+   * the QueryClientProvider to pass data to the
+   * app using React Query.
+   */
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Pages.Dashboard} />
-      </Switch>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Pages.Dashboard} />
+        </Switch>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
