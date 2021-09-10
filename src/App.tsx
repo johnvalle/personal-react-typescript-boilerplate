6ddad21fd24f6390a849ee2cdc2a7663a5ee4193
John Valle <vallejohn.vj@gmail.com>
2021-09-10 23:04:01 +0800

    Add queryClient and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import * as Pages from "./pages";
 
 function App() {
+  /**
+   * Create a new instance of queryClient
+   * and pass it on to the QueryClientProvider
+   * to access data throughout the App using React Query.
+   */
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Pages.Dashboard} />
-      </Switch>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Pages.Dashboard} />
+        </Switch>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
